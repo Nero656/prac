e5dbae5c4f3ff079df949ecc6b38092eaa8dcd4b
koonyaka <57448154+koonyaka@users.noreply.github.com>
2020-03-03 20:45:16 +0700

    adding settings

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -3,4 +3,5 @@ require_once 'core/model.php';
 require_once 'core/view.php';
 require_once 'core/controller.php';
 require_once 'core/route.php';
+require_once 'core/settings.php';
 Route::start(); // запускаем маршрутизатор

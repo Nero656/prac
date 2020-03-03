783699566b867f3e1f02a82378b104e1b6f3f6e3
koonyaka <57448154+koonyaka@users.noreply.github.com>
2020-03-03 18:18:25 +0700

    portfolio model

--- application/models/model_portfolio.php
+++ application/models/model_portfolio.php
@@ -2,25 +2,16 @@
 
 class Model_Portfolio extends Model
 {
-	
-	public function get_data()
-	{	
-				
-		return array(
-			
-			array(
-				'Year' => '2012',
-				'Site' => 'http://DunkelBeer.ru',
-				'Description' => 'Промо-сайт темного пива Dunkel от немецкого производителя Löwenbraü выпускаемого в России пивоваренной компанией "CАН ИнБев".'
-			),
+    public function get_data()
+    {
 
-			array(
-				'Year' => '2012',
-				'Site' => 'http://ZopoMobile.ru',
-				'Description' => 'Русскоязычный каталог китайских телефонов компании Zopo на базе Android OS и аксессуаров к ним.'
-			),
+        $sql = 'SELECT Id, Year, Site, Description FROM porfolio';
 
-		);
-	}
+        $result = mysqli_query($this->link, $sql);
+
+        return $result;
+
+    }
 
 }
+

3fdb26977893734a58bf1adac0471dbf5557cda1
koonyaka <57448154+koonyaka@users.noreply.github.com>
2020-03-06 17:14:40 +0700

    portfolio view fix

--- application/views/portfolio_view.php
+++ application/views/portfolio_view.php
@@ -1,15 +1,26 @@
 <h1>Портфолио</h1>
 <p>
 <table class="table table-bordered">
-Все проекты в следующей таблице являются вымышленными, поэтому даже не пытайтесь перейти по приведенным ссылкам.
-<tr><td>Год</td><td>Проект</td><td>Описание</td></tr>
-<?php
+    Все проекты в следующей таблице являются вымышленными, поэтому даже не пытайтесь перейти по приведенным ссылкам.
+    <tr>
+        <td>Год</td>
+        <td>Проект</td>
+        <td>Описание</td>
+    </tr>
+    <br>
+    <br>
+    <?php
 
-	foreach($data as $row)
-	{
-		echo '<tr><td>'.$row['Year'].'</td><td>'.$row['Site'].'</td><td>'.$row['Description'].'</td></tr>';
-	}
-	
-?>
+    while ($row = mysqli_fetch_assoc($data)) {
+        ?>
+        <tr>
+            <td><?= $row['year'] ?></td>
+            <td><?= $row['site'] ?></td>
+            <td><?= $row['description'] ?></td>
+        </tr>
+        <?php
+    }
+
+    ?>
 </table>
 </p>
